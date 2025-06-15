485d5af3b76c00d822805e7296fd4c5aed330327
VedeVpered <dreamway.agency01@gmail.com>
2025-06-16 01:32:41 +0300

    перезалил

--- pages/index_env.js
+++ pages/index_env.js
@@ -8,7 +8,7 @@ export default function Home() {
 
   const handlePublish = async () => {
     try {
-      const res = await fetch('https://dreampostback-production.up.railway.app/post', {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/post`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ post, image, date, time })
